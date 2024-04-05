8a521b3beac5b1f0c33b759d971b638f22433d70
Md. Jafar Iqbal <45736076+zeesaan@users.noreply.github.com>
2024-04-05 15:51:24 +0600

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,17 +1,1 @@
-#!/bin/bash
 
-# Start rpcbind and rpc-statd
-rpcbind
-rpc.statd
-
-# Mount NFS shares defined in /etc/fstab
-mount -a -t nfs
-
-# Check if any mounts failed
-if [ $? -ne 0 ]; then
-    echo "ERROR: Failed to mount NFS shares from /etc/fstab."
-    exit 1
-fi
-
-# Keep the container running
-tail -f /dev/null
